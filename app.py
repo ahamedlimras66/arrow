3e210c37c78a02ab30f3d26f449e01c383b24e65
mmblack4 <mm4.muthumani@gmail.com>
2020-04-24 01:05:13 +0530

    sitemap

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
 from models.schema import *
 from flask_admin import Admin, AdminIndexView
-from flask import Flask, render_template, send_file, request, url_for, redirect,make_response
+from flask import Flask, render_template, send_file, request, url_for, redirect, send_from_directory
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
-
-app = Flask(__name__, static_folder='static', static_url_path='')
+app = Flask(__name__,static_folder='static')
 app.secret_key = 'my-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -48,17 +47,14 @@ admin.add_view(MyModelView(User, db.session))
 admin.add_view(MyModelView(ExamLink, db.session))
 admin.add_view(MyModelView(Number, db.session))
 
+
 @app.route('/sitemap.xml')
 def static_from_root():
-	response = make_response(open('static/sitemap.xml').read())
-	response.headers["Content-type"] = "text/plain"
-	return response
-
+    return send_from_directory(app.static_folder, request.path[1:])
 # Home page
 @app.route('/')
 def home():
 	return render_template("index.html")
-
 @app.route('/save_number', methods=['POST', 'GET'])
 def save_number():
 	number = Number(number=request.form['number'],name="unknow")
